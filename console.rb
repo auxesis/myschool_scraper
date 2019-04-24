f5ae7e1644a3d7ff67fc90edbf1fa8ad50f59f70
Lindsay Holmwood <lindsay@holmwood.id.au>
2019-04-25 00:19:30 +1000

    Clean up includes and rspec usage

--- console.rb
+++ console.rb
@@ -3,7 +3,6 @@ require 'active_support/inflector'
 require 'csv'
 require 'pry'
 require 'mechanize'
-$LOAD_PATH << File.dirname(__FILE__)
-require 'scraper'
+require_relative './scraper'
 
 binding.pry
