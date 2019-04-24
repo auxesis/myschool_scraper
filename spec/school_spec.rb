a8b9adeaa2cdb04cb3d87be0bb750a5124476882
Lindsay Holmwood <lindsay@holmwood.id.au>
2019-04-25 00:42:59 +1000

    Add tests for scraping

--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -1,8 +1,23 @@
 require 'spec_helper'
 
 describe School do
+  before(:each) do
+    School.source_filename = 'spec/schools.csv'
+  end
+
+  describe '#scrape' do
+    before(:each) do
+      School.scrape
+    end
+
+    it 'reads a csv' do
+      expect(School.records).to_not be_empty
+    end
+  end
+
   describe '#save' do
     before(:each) do
+      School.scrape
       School.save
     end
 
